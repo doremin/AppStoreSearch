947e3a37342e590bccb551e15fb13a29bff1c955
Domin Kim <doremin98@gmail.com>
2024-02-01 08:45:16 +0900

    feat: Add AppStoreAPI Moya Target

---
+++ AppStoreSearch/Networking/AppStoreAPI.swift
@@ -0,0 +1,55 @@
+//
+//  AppStoreAPI.swift
+//  AppStoreSearch
+//
+//  Created by doremin on 2/1/24.
+//
+
+import Foundation
+
+import Moya
+
+enum AppStoreAPI {
+  case search(query: String, media: String, limit: Int, country: String)
+}
+
+extension AppStoreAPI: TargetType {
+  var baseURL: URL {
+    guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "BaseURL") as? URL else {
+      fatalError("BaseURL in plist is empty")
+    }
+    
+    return baseURL
+  }
+  
+  var path: String {
+    switch self {
+    case .search:
+      return "/search"
+    }
+  }
+  
+  var method: Moya.Method {
+    switch self {
+    case .search:
+      return .get
+    }
+  }
+  
+  var task: Moya.Task {
+    switch self {
+    case let .search(query, media, limit, country):
+      let parameters: [String: Any] = [
+        "term": query,
+        "media": media,
+        "limit": limit,
+        "country": country
+      ]
+      return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+    }
+  }
+  
+  var headers: [String : String]? {
+    return nil
+  }
+}
